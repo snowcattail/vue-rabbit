a5e07792db48c7a62deb47aa4115f50631e98f72
snowcattail <2218203282@qq.com>
2026-03-24 16:09:57 +0800

    Axios安装和简单封装

---
+++ .history/src/main_20260324160542.ts
@@ -0,0 +1,17 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+// 测试接口函数
+import { getCategoryAPI } from './apis/testAPI'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
---
+++ .history/src/main_20260324160615.ts
@@ -0,0 +1,20 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+// 测试接口函数
+import { getCategoryAPI } from './apis/testAPI'
+getCategoryAPI().then(res=>{
+    console.log(res);
+})
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')

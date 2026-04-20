a584fde84f3f7aa39b8d5c79506f9749fcb876be
snowcattail <2218203282@qq.com>
2026-04-20 21:36:12 +0800

    切换地址-地址切换交互

--- src/views/Checkout/index.vue
+++ src/views/Checkout/index.vue
@@ -102,7 +102,13 @@
   <!-- 切换地址 -->
   <el-dialog v-model="showDialog" title="切换收货地址" width="30%" center>
     <div class="addressWrapper">
-      <div class="text item" v-for="item in checkInfo.userAddresses" :key="item.id">
+      <div
+        class="text item"
+        :class="`active: ${item.id === activeAddress.id}`"
+        @click="switchAddress(item)"
+        v-for="item in checkInfo.userAddresses"
+        :key="item.id"
+      >
         <ul>
           <li>
             <span>收<i />货<i />人：</span>{{ item.receiver }}
@@ -115,11 +121,10 @@
     <template #footer>
       <span class="dialog-footer">
         <el-button>取消</el-button>
-        <el-button type="primary">确定</el-button>
+        <el-button type="primary" @click="confirm">确定</el-button>
       </span>
     </template>
   </el-dialog>
-
   <!-- 添加地址 -->
 </template>
 
@@ -131,6 +136,16 @@ const checkInfo = ref({}); // 订单对象
 const curAddress = ref({}); // 地址对象
 // 控制弹窗打开
 const showDialog = ref(false);
+// 切换地址
+const activeAddress = ref({});
+const switchAddress = (item) => {
+  activeAddress.value = item;
+};
+// 覆盖地址
+const confirm = () => {
+  curAddress.value = activeAddress.value;
+  showDialog.value = false;
+};
 
 const getCheckInfo = async () => {
   const res = await getCheckInfoAPI();
